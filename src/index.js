c2d6bcdbbdf1f28346428dac8d627fe36ceb15a1
marco <marcomaraglino5@gmail.com>
2023-09-23 20:54:24 +0200

    initial commit

--- src/index.js
+++ src/index.js
@@ -6,10 +6,9 @@ import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 function SocialMedia() {
     return(<div className="socialnetwork">
-            <a href="https://www.instagram.com" className="instagram"><i className="fa fa-instagram"></i></a>
-            <a href="#" className="twitter"><i className="fa fa-twitter"></i></a>
-            <a href="#" className="linkedin"><i className="fa fa-linkedin"></i></a>
-            <a href="#" className="github"><i className="fa fa-github"></i></a>
+            <a href="https://www.instagram.com/marcomaraglino" target="_blank" className="instagram"><i className="fa fa-instagram"></i></a>
+            <a href="https://www.linkedin.com/in/marcomaraglino5/" className="linkedin" target="_blank"><i className="fa fa-linkedin"></i></a>
+            <a href="https://github.com/aushot" className="github" target="_blank"><i className="fa fa-github"></i></a>
         </div>
     )
 }
