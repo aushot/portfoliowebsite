3739fd2f4b5a18ef337a1b2080402447f8c32496
marco <marcomaraglino5@gmail.com>
2023-11-08 22:55:59 +0100

    bug fixes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,6 @@
 import "./App.css"
 import React, {useEffect, useRef, useState} from "react";
 
-function FadeInSection(props) {
-    const [isVisible, setVisible] = React.useState(true);
-    const domRef = React.useRef();
-    React.useEffect(() => {
-        const observer = new IntersectionObserver(entries => {
-            entries.forEach(entry => setVisible(entry.isIntersecting));
-        });
-        observer.observe(domRef.current);
-        return () => observer.unobserve(domRef.current);
-    }, []);
-    return (
-        <div
-            className={`fade-in-section ${isVisible ? 'is-visible' : ''}`}
-            ref={domRef}
-        >
-            {props.children}
-        </div>
-    );
-}
 function Title() {
     return (
         <div className="Title">
